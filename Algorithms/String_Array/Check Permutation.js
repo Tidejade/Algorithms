6dee1cc1798f71ab87424ac68353c2f0e81afd43
Tidejade <tidejade@gmail.com>
2018-01-15 12:02:34 +0900

    Update the solution

--- Algorithms/String_Array/Check Permutation.js
+++ Algorithms/String_Array/Check Permutation.js
@@ -1,4 +1,35 @@
 /**
 Given two Strings, write a mothod to decide if one is a permutation of the other.
+**/
+
+/**
+JAVA SOLUATION 1:
+
+public static boolean CheckPermutation(String str1,String str2){
+
+    if(str1.length()!=str2.length()) return false;
+    for(int i=0;i<str1.length();i++){
+        int index=str1.length()-1-i;
+        if(str1.charAt(i)!=str2.charAt(index))return false;
+    }
+    return true;
+}
+
+JAVA SOLUATION 2:
+public static String sort(String s){
+     char[] content=s.toCharArray();
+     java.util.Arrays.sort(content);
+     return new String(content);
+}
+public static boolean permutation(String s, String t){
+     if(s.length()!= t.length()) return false;
+     return sort(s).equals(sort(t));
+}
 
 **/
+var CheckPermutation=function (str1,str2) {
+  if(str1.length!=str2.length) return false;
+  var char1=str1.split("").sort().toString();
+  var char2=str2.split("").sort().toString();
+  return (char1==char2);
+}
